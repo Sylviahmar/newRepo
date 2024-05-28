21fc8b5e9571de2958edf44b56e6481aa8587568
aseshghatani <aseshghatani4@gmail.com>
2024-05-28 11:51:31 +0530

    fail

--- prg.java
+++ prg.java
@@ -1,7 +1,8 @@
-package newdr;
+package newdr
 
 public class prg {
-   public static void main(String[] args) {
+   public static void main(String[] args) 
+{
     int a=10;
     int b=11;
     int c=a+b;
@@ -10,7 +11,5 @@ public class prg {
     System.out.println("sum: "+c);
       System.out.println("sum of c and a= "+d);
       System.out.println(" "+e);
-   }
-    
-    
+   }    
 }
